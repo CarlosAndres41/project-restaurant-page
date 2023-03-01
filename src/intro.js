29f36050554e28363090690010803fb5ed916de1
CarlosAndres <solrak41@gmail.com>
2023-03-01 18:06:42 -0500

    Add styling to intro.js elements

--- src/intro.js
+++ src/intro.js
@@ -1,9 +1,23 @@
-import bg from './background.jpg';
-
 function intro() {
     const intro = document.createElement('div');
     intro.classList.add('intro');
-    // intro.style.backgroundImage = `url(${bg})`;
+
+    const welcome = document.createElement('h2');
+    welcome.classList.add('welcome-text');
+    welcome.textContent = 'Welcome to';
+
+    const restaurantName = document.createElement('h1');
+    restaurantName.classList.add('restaurant-text');
+    restaurantName.textContent = "Restaurant's name";
+
+    const attribution = document.createElement('a');
+    attribution.classList.add('attribution');
+    attribution.setAttribute('href', 'https://unsplash.com/photos/DCmUhk54F6M');
+    attribution.textContent = 'Image by Piotr Szulawski at Unsplash.com';
+
+    intro.appendChild(welcome);
+    intro.appendChild(restaurantName);
+    intro.appendChild(attribution);
 
     return intro;
 }
