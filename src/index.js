66b1af914b515a42409508c9a956beffb35cdfd6
CarlosAndres <solrak41@gmail.com>
2023-03-01 23:14:45 -0500

    Create menu.js file and import it to index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import './style.css';
 import navBar from './nav-bar.js';
 import intro from './intro.js';
 import aboutUs from './about-us.js';
+import menu from './menu.js';
 
 // Content div:
 
@@ -9,3 +10,4 @@ const cont = document.querySelector('#content');
 cont.appendChild(navBar());
 cont.appendChild(intro());
 cont.appendChild(aboutUs());
+cont.appendChild(menu());
---
+++ src/menu.js
@@ -0,0 +1,8 @@
+function menu() {
+    const menu = document.createElement('div');
+    menu.classList.add('menu-div');
+
+    return menu;
+}
+
+export default menu;
