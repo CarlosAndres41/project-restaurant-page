ca1e4bf327eaa2409b5d8b9dbe49e17cfc872adc
CarlosAndres <solrak41@gmail.com>
2023-03-01 19:43:09 -0500

    Create about-us.js file

---
+++ src/about-us.js
@@ -0,0 +1,8 @@
+function aboutUs() {
+    const aboutUs = document.createElement('div');
+    aboutUs.classList.add('about-us');
+
+    return aboutUs;
+}
+
+export default aboutUs;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import './style.css';
 import navBar from './nav-bar.js';
 import intro from './intro.js';
+import aboutUs from './about-us.js';
 
 // Content div:
 
 const cont = document.querySelector('#content');
 cont.appendChild(navBar());
 cont.appendChild(intro());
+cont.appendChild(aboutUs());
